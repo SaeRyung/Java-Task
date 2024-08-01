5199386eb578312bad45fbf6ae9559b59da9fb2f
SaeRyung <sr1094@naver.com>
2024-08-01 15:06:42 +0900

    1. level02-normal-Application2

---
+++ src/main/java/com/ohgiraffers/section01/conditional/level02/Application2.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section01.conditional.level02;
+
+import java.util.Scanner;
+
+public class Application2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("체중을 입력하세요: ");
+        int kg = scanner.nextInt();
+        System.out.print("키를 입력하세요: ");
+        int tall = scanner.nextInt();
+
+        float BMI = kg / (tall * tall);
+
+        if(BMI<20){
+            System.out.println("당신은 저체중 입니다.");
+        } else if (20 <= BMI && BMI< 25) {
+            System.out.println("당신은 정상 체중 입니다.");
+        } else if(25 <= BMI && BMI < 30){
+            System.out.println("당신은 과체중 입니다.");
+        } else {
+            System.out.println("당신은 비만 입니다.");
+        }
+
+    }
+    }
+
