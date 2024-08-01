25fddf7a83c67275da2bf11b24e95f9e095d5397
SaeRyung <sr1094@naver.com>
2024-08-01 14:46:25 +0900

    1. level01-basic

---
+++ src/main/java/com/ohgiraffers/section01/conditional/level01/basic.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.conditional.level01;
+
+import java.util.Scanner;
+
+public class basic {
+    public class Application1{
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int num = input.nextInt();
+            if(num > 0){
+                System.out.println("양수다.");
+            }
+        }
+    }
+
+}
