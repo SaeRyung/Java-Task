20a1281e885495f7b6a3895214691397fb1133b3
SaeRyung <sr1094@naver.com>
2024-08-02 14:44:05 +0900

    array-level02-normal-Application1_문자열 입력 받은 후 문자 입력 시 문자열 내에 있는 입력한 문자 갯수 세기

--- src/main/java/com/ohgiraffers/section01/arrary/level02/Application1.java
+++ src/main/java/com/ohgiraffers/section01/arrary/level02/Application1.java
@@ -1,25 +1,28 @@
 package com.ohgiraffers.section01.arrary.level02;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Application1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("문자열을 하나 입력하세요 : ");
-        String str = sc.nextLine();
+        String[] str = sc.nextLine().split("");
 
         System.out.print("검색할 문자를 입력하세요 : ");
-        char ch = sc.next().charAt(0);
+//        char[] carr = sc.next().toCharArray();
+        String carr = sc.next();
 
         int count = 0;
 
-        for(int i=0;i<str.length();i++){
-            if(ch==str.charAt(i)){
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals(carr)) {
                 count++;
             }
         }
-        System.out.println("입력하신 문자열 " + str + "에서 찾으시는 문자 " + ch + "은 " + count + "개 입니다." );
 
+        String s = String.join("", str);
 
+        System.out.println("입력하신 문자열 " + s + "에서 찾으시는 문자 " + carr + "은 " + count + "개 입니다.");
     }
 }
