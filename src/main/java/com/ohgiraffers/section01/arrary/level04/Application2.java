23d0985d85d59d1c8bdbe06e1fb4988fa2dd4cc2
SaeRyung <sr1094@naver.com>
2024-08-02 16:25:13 +0900

    array-level04-advanced-Application2_야구게임_수정중

---
+++ src/main/java/com/ohgiraffers/section01/arrary/level04/Application2.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section01.arrary.level04;
+
+import java.util.*;
+
+public class Application2 {
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+
+        label:
+        for(int i=0; i<arr.length; i++) {
+            int num = (int)(Math.random() * 9)+0;
+            arr[i] = num;
+            for(int j=0; j<arr.length; j++) {
+                if(arr[j] == num){
+                    continue label;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
