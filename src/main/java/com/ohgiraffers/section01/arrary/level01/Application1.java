56cf5bdcf53df44155de589640cf25beb9ac7553
SaeRyung <sr1094@naver.com>
2024-08-02 14:15:10 +0900

    array-level01-basic-Application1_길이가 10인 정수형 배열 선언, 각 인덱스에 차례대로 1~10 넣고 출력

---
+++ src/main/java/com/ohgiraffers/section01/arrary/level01/Application1.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section01.arrary.level01;
+
+import java.util.Arrays;
+
+public class Application1 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+1;
+        }
+
+        System.out.print(Arrays.toString(arr));
+    }
+}
