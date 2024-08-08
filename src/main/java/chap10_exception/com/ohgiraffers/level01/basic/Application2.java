3f6211df837aba4755a42ab0babf733e468ba8d7
SaeRyung <101532588+SaeRyung@users.noreply.github.com>
2024-08-08 23:14:53 +0900

    chap10-exception Application2 구현중

--- src/main/java/chap10_exception/com/ohgiraffers/level01/basic/Application2.java
+++ src/main/java/chap10_exception/com/ohgiraffers/level01/basic/Application2.java
@@ -1,17 +1,44 @@
 package chap10_exception.com.ohgiraffers.level01.basic;
 
+import java.time.LocalDate;
 import java.util.*;
 
 
 public class Application2 {
     public static void main(String[] args) {
-        InvalidAgeException ageException = new InvalidAgeException();
+//        InvalidAgeException ageException = new InvalidAgeException();
 
         Scanner input = new Scanner(System.in);
         System.out.print("생년월일 입력 (yyyy-MM-dd 양식으로 작성) : ");
         String HBD = input.nextLine();
 
-        ageException.checkHBD(HBD);
+
+
+        try{
+            InputBirthDay(HBD);
+        }catch(InvalidAgeException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void InputBirthDay(String HBD){
+        LocalDate today = LocalDate.now();
+        int year = today.getYear();
+
+        int age = Integer.parseInt(HBD.substring(0, 4));
+        int mm = Integer.parseInt(HBD.substring(4, 6));
+        int dd = Integer.parseInt(HBD.substring(6, 8));
+
+        if(year - age < 20){
+            System.out.println("만 20세 이상은 입장 불가입니다.");
+        }
+        if(year - age > 20){
+            System.out.println("입장하셔도 됩니다.");
+        }if(mm>12 || mm<1){
+            throw new InvalidAgeException();
+        }if(dd>31 || dd<1){
+            throw new InvalidAgeException();
+        }
 
     }
 
