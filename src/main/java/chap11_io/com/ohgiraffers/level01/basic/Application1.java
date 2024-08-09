616d6d8dd435fadabbfc2974b19fa193fe12e2b8
SaeRyung <sr1094@naver.com>
2024-08-09 17:37:46 +0900

    chap11-io-파일 읽기 예외 처리 구현중, 파일 읽어오기 중 결과값이 숫자로 나옴

--- src/main/java/chap11_io/com/ohgiraffers/level01/basic/Application1.java
+++ src/main/java/chap11_io/com/ohgiraffers/level01/basic/Application1.java
@@ -1,19 +1,32 @@
 package chap11_io.com.ohgiraffers.level01.basic;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Application1 {
     public static void main(String[] args) {
 
 //        FileInputStream inputFile = null;
 
-        try(FileReader inputFile = new FileReader("hello.txt")){
-            char[] arr = new char[100];
-            inputFile.read();
+//        try(FileReader inputFile = new FileReader("hello.txt")){
+////            char[] arr = new char[100];
+//            System.out.println(String.valueOf(inputFile.read()));
+//
+//
+////            for(char c : arr) System.out.print(c);
+//        } catch (IOException e) {
+//            System.out.println("오류 : 해당 이름을 가진 파일이 없습니다.");
+//        }
 
-            for(char c : arr) System.out.print(c);
+        FileInputStream fis = null;
+
+        try{
+            fis = new FileInputStream("hello.txt");
+            byte[] bar = new byte[100];
+            fis.read(bar);
+            System.out.println(Arrays.toString(bar));
         } catch (IOException e) {
-            System.out.println("해당 이름을 가진 파일이 없습니다.");
+            System.out.println("오류 : 해당 이름을 가진 파일이 없습니다.");
         }
 
     }
